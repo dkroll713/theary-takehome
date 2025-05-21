3ca4801849e104d84e6ccb5be85e84564683bf67
David Kroll <dkroll713@gmail.com>
2025-05-21 16:28:29 -0500

    cleaned some outdated code

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,7 @@
 import express from "express";
-import db from "./repository/sqliteRepository.js";
-import { Root, Node } from "./interfaces/interfaces.js"
-import createTreeNodeSchema from "./schemas/CreateTreeNodeSchema.js";
 import treeRoutes from "./routes/treeRoutes.js";
 
 const app = express();
-
-const PORT = 3000;
 
 app.use(express.json());
 
